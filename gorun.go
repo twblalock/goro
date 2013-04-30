18c4e266d0bf9ce81c9444d2d5721e44f471c28b
Tyler Blalock <twblalock@gmail.com>
2013-04-30 12:05:25 -0700

    Fixed formatting issues

--- gorun.go
+++ gorun.go
@@ -29,48 +29,47 @@ func main() {
     path, err := exec.LookPath(cmdName)
     if nil != err {
         fmt.Fprint(os.Stderr, err)
-	os.Exit(1)
+        os.Exit(1)
     }
-
-    runtime.GOMAXPROCS(runtime.NumCPU())
-    sem := make(chan int, *maxGr)
-    out := make(chan []byte)
 
     var allArgs []string
     if len(*fileName) != 0 {
         content, err := ioutil.ReadFile(*fileName)
         if nil != err {
             fmt.Fprint(os.Stderr, err)
-	    os.Exit(1)
+            os.Exit(1)
         } else {
- 	        allArgs = strings.Split(strings.Trim(string(content), "\n"), "\n")
-	    }
+            allArgs = strings.Split(strings.Trim(string(content), "\n"), "\n")
+        }
     } else {
         allArgs = flag.Args()[1:]
     }
 
+    runtime.GOMAXPROCS(runtime.NumCPU())
+    sem := make(chan int, *maxGr)
+    out := make(chan []byte)
     for i := 0; i < len(allArgs); i++ {
-	args := strings.Split(allArgs[i], " ")
-	// Append prefix
-	if len(*prefix) != 0 {
-	    args = append(strings.Split(*prefix, " "), args...)
-    }
-    args = append([]string{cmdName}, args...)
-	// Append suffix
-	if len(*suffix) != 0 {
-        args = append(args, strings.Split(*suffix, " ")...)
-    }
-    
-    cmd := exec.Cmd{Path: path, Args: args}
-	go func(cmd exec.Cmd) {
-	    sem <- 1
-	    cmdOutput, _ := cmd.CombinedOutput()
-	    out <- cmdOutput
-	    <- sem
+        args := strings.Split(allArgs[i], " ")
+        // Append prefix
+        if len(*prefix) != 0 {
+            args = append(strings.Split(*prefix, " "), args...)
+        }
+        args = append([]string{cmdName}, args...)
+        // Append suffix
+        if len(*suffix) != 0 {
+            args = append(args, strings.Split(*suffix, " ")...)
+        }
+
+        cmd := exec.Cmd{Path: path, Args: args}
+        go func(cmd exec.Cmd) {
+            sem <- 1
+            cmdOutput, _ := cmd.CombinedOutput()
+            out <- cmdOutput
+            <-sem
         }(cmd)
     }
 
     for _ = range allArgs {
-        fmt.Fprintf(os.Stdout, "%s", <- out)
+        fmt.Fprintf(os.Stdout, "%s", <-out)
     }
 }
